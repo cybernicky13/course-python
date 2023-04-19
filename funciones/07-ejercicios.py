7057329ab0d4dc54af41e222b249cc4a03e45b37
CyberNicky13 <nickydelvalle13@gmail.com>
2023-04-19 17:59:25 -0600

    Finalice ahora si xd, los ejercicios de funciones

--- funciones/07-ejercicios.py
+++ funciones/07-ejercicios.py
@@ -3,11 +3,14 @@
 
 def es_palindormo(texto):
     """Esta es una funcion para saber si un string es palindromo"""
-    string_reverso = texto[::-1]
+    texto = texto.replace(" ", "")
+    string_reverse = texto[::-1]
+    print(texto)
 
-    is_palindromo = True if string_reverso == texto else False
+    is_palindromo = True if string_reverse == texto else False
     return is_palindromo
 
 
 print("abba", es_palindormo("abba"))
+print("amo la paloma", es_palindormo("amo la paloma"))
 print("rock", es_palindormo("rock"))
