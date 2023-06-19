ee5eda821df651ed25df74662edb33572802cd01
Nicolás Del Valle <121812858+CyberNicky13@users.noreply.github.com>
2023-06-19 16:12:34 -0600

    Finalice la seccion paquetes-populares

--- paquetes-populares/04-scrapper.py
+++ paquetes-populares/04-scrapper.py
@@ -6,4 +6,11 @@ respuesta = requests.get(url)
 texto = respuesta.text
 soup = BeautifulSoup(texto,"html.parser")
 
-soup.select("")
+preguntas = soup.select(".s-post-summary")
+
+print(preguntas[0]["data-post-id"])
+
+for pregunta in preguntas:
+    titulo = pregunta.select_one(".s-link").get_text()
+    usuario = pregunta.select_one(".s-user-card--link").get_text()
+    # print(f"{usuario.strip()} - Titulo: \n{titulo.strip()}")
